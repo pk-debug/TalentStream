8851b57fbf5e262babcbd2c9706c2203eab71dfd
pawan-flutter <coder.pawan1@gmail.com>
2026-09-08 22:36:28 +0530

    minor fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+
+subprojects {
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.ndkVersion = "27.0.12077973"
+        }
+    }
 }
